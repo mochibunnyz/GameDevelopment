f66c64ef7147e721fb5e2cf9ff13cfd238f94b55
Mavis Heng Lee Xuan <106320398+mochibunnyz@users.noreply.github.com>
2023-08-15 23:42:39 +0800

    basic step code

--- Game Development/Assets/Scripts/LightUpStep.cs
+++ Game Development/Assets/Scripts/LightUpStep.cs
@@ -5,18 +5,34 @@ using UnityEngine;
 public class LightUpStep : MonoBehaviour
 {
     private Renderer rend;
+    private bool hasSteppedOnce;
+    public int whiteStepNum;
+    public bool hasBlackStep; 
     // Start is called before the first frame update
     void Start()
     {
         rend = GetComponent<Renderer>();
+        hasSteppedOnce = false;
+        hasBlackStep = false;
+
     }
 
     private void OnTriggerEnter(Collider collider)
     {
-        if(collider.tag == "Player")
+        if(collider.tag == "Player" && hasSteppedOnce == false)
         {
-            rend.material.color = Color.yellow;
+            rend.material.color = Color.white;
+            hasSteppedOnce = true;
+            whiteStepNum++; 
         }
+
+        if (collider.tag == "Player" && hasSteppedOnce == true)
+        {
+            rend.material.color = Color.black;
+            hasBlackStep = false; 
+        }
+
+
     }
 
     // Update is called once per frame
