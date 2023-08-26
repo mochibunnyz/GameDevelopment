5961ad474135b62508cd2971f3321ea37aff036f
Mavis Heng Lee Xuan <106320398+mochibunnyz@users.noreply.github.com>
2023-08-27 00:59:15 +0800

    complete  tile puzzle and edit treasure

--- Game Development/Assets/Scripts/PuzzleTreasureTouch.cs
+++ Game Development/Assets/Scripts/PuzzleTreasureTouch.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TreasureTouch : MonoBehaviour
+public class PuzzleTreasureTouch : MonoBehaviour
 {
     public GameObject treasuretouch;
+    public bool hasBlackStep;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -15,16 +17,22 @@ public class TreasureTouch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (stepPuzzle.hasBlackStep == true)
+        {
+            
+            Debug.Log("Puzzle Treasure Destroyed");
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player")
+        if (other.tag == "Player")
         {
-            Destroy(gameObject);
             CharacterControl.numberOfTreasures += 1;
             Debug.Log("Treasures Obtained:" + CharacterControl.numberOfTreasures);
+            Destroy(gameObject);
+            
             treasureTouch();
 
         }
@@ -34,6 +42,5 @@ public class TreasureTouch : MonoBehaviour
     {
         treasuretouch.SetActive(true);
     }
-
 
 }
