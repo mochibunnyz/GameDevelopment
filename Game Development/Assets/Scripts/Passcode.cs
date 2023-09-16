248dd3a81e225a267e53b6d7fd8b760d65b7678d
chingchieh9000 <chingchiehng@gmail.com>
2023-09-16 13:23:57 +0800

    Changed the puzzle 4 logics and added more signs

--- Game Development/Assets/Scripts/Passcode.cs
+++ Game Development/Assets/Scripts/Passcode.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class Passcode : MonoBehaviour
 {
@@ -15,8 +16,13 @@ public class Passcode : MonoBehaviour
     string Code = "302";
     string Nr = null;
     int NrIndex = 0;
+    int WrongCounter = 0;
+    int Case = 0;
     string aplha;
     public Text UiText = null;
+    string TheCode = "HEW";
+    public Text CodeText;
+    public TextMeshProUGUI OnSceneCode_Text;
 
     public void CodeFunction(string Numbers)
     {
@@ -27,20 +33,54 @@ public class Passcode : MonoBehaviour
 
     public void Enter()
     {
-        if(Nr == Code)
+        if (Nr == Code)
         {
             UiText.text = "Correct";
             StartCoroutine(PuzzleSolved());
             Cursor.visible = false;
             Destroy();
-            
+
 
         }
-        else if(Nr != Code)
+        else if (Nr != Code)
         {
             UiText.text = "Try again";
             StartCoroutine(PuzzleWrong());
+            WrongCounter++;
 
+        }
+
+        if (Nr != Code & WrongCounter == 3 & Case == 0)
+        {
+            UiText.text = "All Attempts used";
+            Code = "581";
+            TheCode = "QVU";
+            CodeText.text = TheCode;
+            OnSceneCode_Text.text = TheCode;
+            WrongCounter = 0;
+            Case++;
+
+        }
+        if (Nr != Code & WrongCounter == 3 & Case == 1)
+        {
+            UiText.text = "All Attempts used";
+            Code = "102";
+            TheCode = "UEW";
+            CodeText.text = TheCode;
+            OnSceneCode_Text.text = TheCode;
+            WrongCounter = 0;
+            Case++;
+
+        }
+        if (Nr != Code & WrongCounter == 3 & Case == 2)
+        {
+            UiText.text = "All Attempts used";
+            Code = "810";
+            TheCode = "VUE";
+            CodeText.text = TheCode;
+            OnSceneCode_Text.text = TheCode;
+            WrongCounter = 0;
+            Case = 0;
 
         }
     }
