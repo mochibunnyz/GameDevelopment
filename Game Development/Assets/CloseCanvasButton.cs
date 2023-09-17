147f6e349ed105bdbc6605f90adc2426f42a5add
Charcoallenz <lancephua93@gmail.com>
2023-09-17 19:16:18 +0800

    collider and hint sign
    fix more collider and added missing scripts for hints

---
+++ Game Development/Assets/CloseCanvasButton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseCanvasButton : MonoBehaviour
+{
+    public Canvas canvasToClose;  // Reference to the canvas you want to close
+
+    public void CloseCanvas()
+    {
+        if (canvasToClose != null)
+        {
+            canvasToClose.enabled = false;
+        }
+        else
+        {
+            Debug.LogError("Canvas reference not set in the CloseCanvasButton script.");
+        }
+    }
+}
