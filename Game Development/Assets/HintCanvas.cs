797d1a7a71bb55500cdafc26de8b2a385844d69e
Charcoallenz <lancephua93@gmail.com>
2023-09-17 17:44:26 +0800

    puzzle hint
    added first sign for puzzle hints

---
+++ Game Development/Assets/HintCanvas.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HintCanvas : MonoBehaviour
+{
+    public Canvas canvasToClose;  // Reference to the canvas you want to close
+
+    public void CloseCanvas()
+    {
+        if (canvasToClose != null)
+        {
+            canvasToClose.enabled = false;
+        }
+        else
+        {
+            Debug.LogError("Canvas reference not set in the CloseCanvasButton script.");
+        }
+    }
+}
